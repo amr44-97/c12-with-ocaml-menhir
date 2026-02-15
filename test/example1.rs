bd0b3aa06f61979f6de51fc7f82d0b83438d8419
Amr Maharek <amrabdo4790@gmail.com>
2026-02-15 18:46:22 +0200

    test syntax

--- test/example1.rs
+++ test/example1.rs
@@ -1,7 +1,22 @@
-include stdio;
+include std.mem;
+include std.io.*;
 
 struct T;
+
+struct Vec3f { 
+    float x=1;
+    float y=1;
+    int A =1;
+}
+
+
+char* read_file(char* file_name) {
+    int[] buffer;
+    return printf(file_name);
+}
+
 
 int main(int argc, char* argv) {
 	let x = 10;
 }
+
